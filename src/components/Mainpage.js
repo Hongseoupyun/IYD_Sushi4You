4f3b5811434f1047d2f62c57861df0e8a9e9d484
Hongseoup Yun <simonhongseoupyun02156@gmail.com>
2023-05-31 00:16:43 -0400

    feat(Mainpage):added button to lead users to ubereats to order online

--- src/components/Mainpage.js
+++ src/components/Mainpage.js
@@ -8,9 +8,17 @@ import { useNavigate } from "react-router-dom";
 export default function Mainpage() {
   const navigate = useNavigate();
 
-  const handleClick = () => {
+  const handleMenuClick = () => {
     navigate("/menu");
   };
+
+  const handleUberClick = () => {
+    window.open(
+      "https://www.ubereats.com/ca/store/sushi-for-you/xONBNyT1T0S8Ck4KOyWl2Q?diningMode=DELIVERY&pl=JTdCJTIyYWRkcmVzcyUyMiUzQSUyMlN1c2hpJTIwNCUyMFUlMjIlMkMlMjJyZWZlcmVuY2UlMjIlM0ElMjJjYmMzYzI4Yy0wZDg3LTcwMzctM2QxNS1hYzhiOWZhYTNlNjQlMjIlMkMlMjJyZWZlcmVuY2VUeXBlJTIyJTNBJTIydWJlcl9wbGFjZXMlMjIlMkMlMjJsYXRpdHVkZSUyMiUzQTQzLjU5MTQ1MDIlMkMlMjJsb25naXR1ZGUlMjIlM0EtNzkuNzQxNDgwOCU3RA%3D%3D&ps=1",
+      "_blank"
+    );
+  };
+
   return (
     <Container>
       <Left>
@@ -28,7 +36,8 @@ export default function Mainpage() {
           <RegularParagraph>11:30AM to 9:30PM</RegularParagraph>
         </Hour>
         <Wrapper>
-          <Menu onClick={handleClick}>Menu</Menu>
+          <Menu onClick={handleMenuClick}>Menu</Menu>
+          <UberButton onClick={handleUberClick}>Order Online</UberButton>
           <Arrow src={Arrow_Menu} />
         </Wrapper>
       </Right>
@@ -72,9 +81,7 @@ const Address = styled.div`
   letter-spacing: 6px; // adjust as necessary
 `;
 
-const Underline = styled.img`
-  width: 80%;
-`;
+const Underline = styled.img``;
 
 const Paragraph = styled.p`
   margin: 0; // removes default margin from paragraphs
@@ -120,10 +127,24 @@ const Menu = styled.div`
     background-color: #05493c; // change the background color on hover
     color: #fdfaf5; // change the text color on hover
     transform: scale(1.1); // scales the button 10% larger on hover
+    font-weight: 600; // change the font weight on hover
   }
 
   &:active {
     transform: scale(0.9); // scales the button 10% smaller on click
+    font-weight: 600; // change the font weight on hover
+  }
+`;
+
+const UberButton = styled(Menu)`
+  // inherit the styles from the Menu component
+  font-size: 1.25rem; // make the font size slightly smaller
+  margin-left: 20px; // add some space between the Menu and UberButton
+  border-color: #eb1c23; // make the border color Uber's red
+  color: #eb1c23; // make the text color Uber's red
+
+  &:hover {
+    background-color: #eb1c23; // make the background color Uber's red on hover
   }
 `;
 const Arrow = styled.img`
