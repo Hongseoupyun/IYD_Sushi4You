a6f865ffaa5091cba5fab44ea4bfe72c5569a40b
Hongseoup Yun <simonhongseoupyun02156@gmail.com>
2023-06-02 14:50:33 -0400

    feat(Firebase): Modified the JSON Data and its script to upload the data automatically

--- scripts/populateDatabase.js
+++ scripts/populateDatabase.js
@@ -1,15 +1,16 @@
-const admin = require("firebase-admin");
-const serviceAccount = require("./sushi4UAdminSdk.json");
-const signatureMenus = require("./signatureData.json");
-// const menuItems = require("./menuData.json");
+// const admin = require("firebase-admin");
+// const serviceAccount = require("./sushi4UAdminSdk.json");
+// // const signatureMenus = require("./signatureData.json");
+// // const menuItems = require("./menuData.json");
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+// });
 
-const db = admin.firestore();
+// const db = admin.firestore();
 
-// let menuItems = [];
+// // let menuItems = [];
+// // let signatureMenus = [];
 
 // menuItems.forEach((item) => {
 //   db.collection("menu")
@@ -18,13 +19,15 @@ const db = admin.firestore();
 //     .catch((error) => {
 //       console.error("Error adding document: ", error);
 //     });
+//     console.log("Menu items added to Firestore!")
 // });
 
-signatureMenus.forEach((item) => {
-  db.collection("signaturemenu")
-    .doc()
-    .set(item)
-    .catch((error) => {
-      console.error("Error adding document: ", error);
-    });
-});
+// signatureMenus.forEach((item) => {
+//   db.collection("signaturemenu")
+//     .doc()
+//     .set(item)
+//     .catch((error) => {
+//       console.error("Error adding document: ", error);
+//     });
+//     console.log("Signature menu items added to Firestore!")
+// });
