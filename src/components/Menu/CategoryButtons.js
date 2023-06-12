f85145f5159c456ff1252d4bc02bf39f7ba18d82
Hongseoup Yun <simonhongseoupyun02156@gmail.com>
2023-06-11 23:07:16 -0400

    modified(Menudata and Menu Page): Componentized and updated datas

---
+++ src/components/Menu/CategoryButtons.js
@@ -0,0 +1,78 @@
+import React from "react";
+import styled from "styled-components";
+
+export default function CategoryButtons(props) {
+  const {
+    categories,
+    selectedCategory,
+    handleCategoryClick,
+    loadingCategories,
+  } = props;
+  return (
+    <Container>
+      {!loadingCategories && (
+        <>
+          <CategoryButton
+            key={"all"}
+            className={selectedCategory === "All" ? "selected" : ""} // Add the 'selected' class when this category is selected
+            onClick={() => handleCategoryClick("All")}
+          >
+            All
+          </CategoryButton>
+          {categories.map((category) => (
+            <CategoryButton
+              key={category}
+              className={selectedCategory === category ? "selected" : ""} // Add the 'selected' class when this category is selected
+              onClick={() => handleCategoryClick(category)}
+            >
+              {category}
+            </CategoryButton>
+          ))}
+        </>
+      )}
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  padding-bottom: 40px;
+  padding-top: 20px;
+  flex-wrap: wrap;
+  height: 25vh;
+  width: 100%;
+  margin-bottom: 50px;
+  background-color: #fdfaf5;
+`;
+
+const CategoryButton = styled.button`
+  background-color: #99c0a3;
+  font-weight: 500; // Set the font-weight to semi-bold by default
+  padding: 10px 20px;
+  border: 2px solid transparent; // Add a transparent border to prevent layout shifts
+  border-radius: 25px;
+  height: 45px;
+  font-size: 1.2em;
+  color: black;
+  min-width: 100px;
+  margin: 5px 10px;
+  cursor: pointer;
+  box-shadow: 0px 8px 15px rgba(0, 0, 0, 0.1);
+  transform: translateY(3px);
+  transition: all 0.3s ease-in;
+
+  &:hover {
+    background-color: #f14e23;
+    transform: translateY(-3px) scale(1.02);
+    color: white;
+    transition: all 0.3s ease-out;
+  }
+
+  &.selected {
+    background-color: #f14e23;
+    transform: translateY(-3px) scale(1.02);
+    color: white;
+    transition: all 0.3s ease-in;
+  }
+`;
--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import Navbar from "components/Navbar";
 import Footer from "components/Footer";
 import MenuItem from "components/Menu/MenuItem";
+import CategoryButtons from "components/Menu/CategoryButtons";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import { collection, getDocs } from "firebase/firestore";
@@ -69,28 +70,13 @@ export default function Menu() {
         handleMenuClick={handleMenuClick}
         setSelectedMenuItem={setSelectedMenuItem}
       />
-      <CategoryButtons>
-        {!loadingCategories && (
-          <>
-            <CategoryButton
-              key={"all"}
-              className={selectedCategory === "All" ? "selected" : ""} // Add the 'selected' class when this category is selected
-              onClick={() => handleCategoryClick("All")}
-            >
-              All
-            </CategoryButton>
-            {categories.map((category) => (
-              <CategoryButton
-                key={category}
-                className={selectedCategory === category ? "selected" : ""} // Add the 'selected' class when this category is selected
-                onClick={() => handleCategoryClick(category)}
-              >
-                {category}
-              </CategoryButton>
-            ))}
-          </>
-        )}
-      </CategoryButtons>
+      <CategoryButtons
+        loadingCategories={loadingCategories}
+        categories={categories}
+        selectedCategory={selectedCategory}
+        setSelectedCategory={setSelectedCategory}
+        handleCategoryClick={handleCategoryClick}
+      />
       <MenuContainer>
         {selectedCategory !== "All" ? (
           <CategoryBlock>
@@ -127,49 +113,6 @@ export default function Menu() {
 
 const Container = styled.div`
   background-color: #f5f5f5;
-`;
-
-const CategoryButtons = styled.div`
-  display: flex;
-  justify-content: center;
-  padding-bottom: 40px;
-  padding-top: 20px;
-  flex-wrap: wrap;
-  height: 25vh;
-  width: 100%;
-  margin-bottom: 50px;
-  background-color: #fdfaf5;
-`;
-
-const CategoryButton = styled.button`
-  background-color: #99c0a3;
-  font-weight: 500; // Set the font-weight to semi-bold by default
-  padding: 10px 20px;
-  border: 2px solid transparent; // Add a transparent border to prevent layout shifts
-  border-radius: 25px;
-  height: 45px;
-  font-size: 1.2em;
-  color: black;
-  min-width: 100px;
-  margin: 5px 10px;
-  cursor: pointer;
-  box-shadow: 0px 8px 15px rgba(0, 0, 0, 0.1);
-  transform: translateY(3px);
-  transition: all 0.3s ease-in;
-
-  &:hover {
-    background-color: #f14e23;
-    transform: translateY(-3px) scale(1.02);
-    color: white;
-    transition: all 0.3s ease-out;
-  }
-
-  &.selected {
-    background-color: #f14e23;
-    transform: translateY(-3px) scale(1.02);
-    color: white;
-    transition: all 0.3s ease-in;
-  }
 `;
 
 const MenuContainer = styled.div`
