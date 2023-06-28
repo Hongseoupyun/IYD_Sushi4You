85ae04dd9e97c572096a39e1774db69c32a9154a
Hongseoup Yun <simonhongseoupyun02156@gmail.com>
2023-06-27 23:02:32 -0400

    refactored(useMenuClick): Refactored the useMenuClick hook

--- src/hooks/useMenuClick.js
+++ src/hooks/useMenuClick.js
@@ -7,22 +7,34 @@ export const useMenuClick = () => {
   const [selectedMenuItem, setSelectedMenuItem] = useState("Menu");
 
   const handleMenuClick = (path) => {
-    if (path === "Home") {
-      navigate("/");
-    } else if (path === "Menu") {
-      navigate(`/${path.toLowerCase()}`);
-    } else if (path === "Location") {
-      navigate("/#location");
-    }
-    if (path !== "Location") {
-      setSelectedMenuItem(path);
-    }
+    // define a dictionary of actions for each path
+    const actions = {
+      Home: () => {
+        navigate("/");
+        setSelectedMenuItem(path);
+        window.scrollTo({ top: 0, behavior: "smooth" });
+      },
+      Menu: () => {
+        navigate("/menu");
+        setSelectedMenuItem(path);
+        window.scrollTo({ top: 0, behavior: "smooth" });
+      },
+      Location: () => {
+        navigate("/#location");
+        setSelectedMenuItem(path);
+      },
+      default: () => {
+        navigate(`/${path.toLowerCase()}`);
+        setSelectedMenuItem(path);
+        window.scrollTo({ top: 0, behavior: "smooth" });
+      },
+    };
+
+    // perform the action based on the path, or the default action if the path is not found
+    (actions[path] || actions.default)();
   };
-  
+
   useEffect(() => {
-    if (location.pathname === "/menu") {
-      window.scrollTo({ top: 0, behavior: "smooth" });
-    }
     if (location.pathname === "/" && location.hash === "#location") {
       // Introducing a delay of 100ms to ensure the "location" element is present in the DOM
       setTimeout(() => {
@@ -30,7 +42,7 @@ export const useMenuClick = () => {
         if (element) {
           window.scrollTo({ top: element.offsetTop, behavior: "smooth" });
         }
-      }, 0);
+      }, 300);
     }
   }, [location]);
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
-import React, { useRef, useEffect, useState } from "react";
-import { useNavigate, useLocation } from "react-router-dom";
+import React, { useRef, useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import { useMenuClick } from "hooks/useMenuClick";
 import Navbar from "components/Navbar";
 import Mainpage from "components/Home/Mainpage";
 import Signature from "components/Home/Signature";
@@ -9,37 +10,15 @@ import Footer from "components/Footer";
 export default function Home() {
   // Initialize references and hooks
   const locationRef = useRef(null);
-  const navigate = useNavigate();
   const location = useLocation();
-  const [selectedMenuItem, setSelectedMenuItem] = useState("Home");
 
   // Function to smoothly scroll to the top of the page
-  const scrollToTop = () => window.scrollTo({ top: 0, behavior: "smooth" });
+  const { handleMenuClick, selectedMenuItem, setSelectedMenuItem } =
+    useMenuClick();
 
-  // Function to handle menu click events
-  const handleMenuClick = (path) => {
-    switch (path) {
-      case "Home":
-        setSelectedMenuItem("Home");
-        scrollToTop();
-        navigate("/");
-        break;
-      case "Location":
-        locationRef.current.scrollIntoView({ behavior: "smooth" });
-        setSelectedMenuItem("Location");
-        break;
-      default:
-        navigate(`/${path.toLowerCase()}`);
-        setSelectedMenuItem(path);
-    }
-  };
-
-  // Effect to scroll to the Location component on page load if the URL hash is '#location'
   useEffect(() => {
-    if (location.hash === "#location") {
-      handleMenuClick("Location");
-    }
-  }, [location.hash]);
+    setSelectedMenuItem(location.hash === "#location" ? "Location" : "Home");
+  }, []);
 
   return (
     <div>
@@ -58,7 +37,3 @@ export default function Home() {
     </div>
   );
 }
-
-
-
-
